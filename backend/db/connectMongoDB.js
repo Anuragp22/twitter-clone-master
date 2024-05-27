f7583d1dcb20cad359b4b2d07cd5ef7de554b115
Anuragp22 <anuragp2003b@gmail.com>
2024-05-27 10:21:28 +0530

    ..

--- backend/db/connectMongoDB.js
+++ backend/db/connectMongoDB.js
@@ -1,13 +1,12 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const connectMongoDB = async () => {
-	try {
-		const conn = await mongoose.connect(process.env.MONGO_URI);
-		console.log(`MongoDB connected: ${conn.connection.host}`);
-	} catch (error) {
-		console.error(`Error connection to mongoDB: ${error.message}`);
-		process.exit(1);
-	}
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+  } catch (error) {
+    console.error(`Error connection to mongoDB: ${error.message}`);
+    process.exit(1);
+  }
 };
 
 export default connectMongoDB;
